11f746eecfd1d251ee907b659df86b122eaf7dc0
JohnnHidalgo <31965496+JohnnHidalgo@users.noreply.github.com>
2019-10-04 19:00:54 -0400

    Add reponse for chat in onUpdateReceived method

--- src/main/java/KajoyBot.java
+++ src/main/java/KajoyBot.java
@@ -1,9 +1,18 @@
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
 public class KajoyBot extends TelegramLongPollingBot {
     public void onUpdateReceived(Update update) {
+        final String messageTextReceived = update.getMessage().getText();
+        final long chatId = update.getMessage().getChatId();
+        SendMessage message = new SendMessage().setChatId(chatId).setText(messageTextReceived);
+        try {
+            execute(message);
+        } catch (TelegramApiException e) {
 
+        }
     }
 
     public String getBotUsername() {

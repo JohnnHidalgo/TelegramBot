bc8ac19837b446ce93d8b63d776712240b9a5160
JohnnHidalgo <31965496+JohnnHidalgo@users.noreply.github.com>
2019-10-04 18:54:30 -0400

    Add TelegramLongPollingBot library in KajoyBot.java

--- src/main/java/KajoyBot.java
+++ src/main/java/KajoyBot.java
@@ -1,4 +1,16 @@
-package PACKAGE_NAME;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.objects.Update;
 
-public class KajoyBot {
+public class KajoyBot extends TelegramLongPollingBot {
+    public void onUpdateReceived(Update update) {
+
+    }
+
+    public String getBotUsername() {
+        return null;
+    }
+
+    public String getBotToken() {
+        return null;
+    }
 }
